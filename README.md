a1719fe02cd7a7b63a1a32f4fe89ae545e52ddc8
toky rasolofo <toky rasolofo>
2017-02-23 23:30:32 +0100

    premieres actions sur symfony

---
+++ README.md
@@ -0,0 +1,4 @@
+test_project
+============
+
+A Symfony project created on February 23, 2017, 9:45 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Article;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function createAction()
+    {
+        
+
+        for ($i=0; $i < 10; $i++) { 
+            $article = new Article();
+            $article->setTitle("article ".rand(1, 1000))->setContent("bonjour 1er article")->setDate(new \Datetime())->setUserId(42);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();    
+        }
+
+        return new Response("creation d'un article"); 
+    }
+
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function articlesAction()
+    {
+        $repo = $this->getDoctrine()->getRepository("AppBundle:Article");
+
+        $articles = $repo->findOneById(22);
+
+        var_dump($articles);
+
+        return new Response();
+    }
+}
