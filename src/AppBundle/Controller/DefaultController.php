a06d2133e504732bcf36ee2a4a52ccb7774fdbf3
naivo03 <naivorasolofo@yahoo.fr>
2017-03-15 18:15:21 +0100

    rajout du templating et du CRUD (on going)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,12 +1,15 @@
-<?php
+<?php 
 
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Article;
+use AppBundle\Form\Type\CreateUpdateFormType    ;
 
 class DefaultController extends Controller
 {
@@ -19,6 +22,48 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
+    }
+
+    /**
+     * @Route("/remove/{idArticle}", defaults={"idArticle" = 0}, name="remove")
+     * @Method("GET")
+     * @Template()
+     */
+    public function removeAction($idArticle)
+    { 
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AppBundle:Article')->find($idArticle);
+
+        $em->remove($product);
+        $em->flush(); //execute la requete demande ici remove(product)
+        return new Response("suppression d'un article"); 
+    }
+
+    /**
+     * @Route("/update", name="update")
+     */
+    public function updateAction(Request $request)
+    {
+        
+        $formulaire = $this->createForm(CreateUpdateFormType::class);
+        $formulaire->handleRequest($request);
+
+        if($formulaire->isValid())
+        {
+            dump($formulaire->getData());
+        }
+
+        return $this->render('article/update.html.twig', ['formulaire' => $formulaire->createView()]);
+
+        /*$em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AppBundle:Article')->find(1);
+
+        dump($product);
+        $product->setTitle('titre updater');
+        $em->persist($product);
+        $em->flush();
+
+        return new Response("update d'un article");*/ 
     }
 
     /**
@@ -36,7 +81,7 @@ class DefaultController extends Controller
             $em->flush();    
         }
 
-        return new Response("creation d'un article"); 
+        return new Response("creation d'articles dans database"); 
     }
 
     /**
@@ -45,9 +90,34 @@ class DefaultController extends Controller
     public function articlesAction()
     {
         $repo = $this->getDoctrine()->getRepository("AppBundle:Article");
-
         $articles = $repo->findAll();
 
-        return $this->render("article/articles.html.twig", array('articles' => $articles));
+        return $this->render("article/articles.html.twig", ['articles' => $articles]);
     }
+
+
+    /**
+     * @Route("/article", name="article")
+     */
+    public function articleAction(/*$idArticle*/) //vue d'un article en particulier
+    {
+        $repo = $this->getDoctrine()->getRepository("AppBundle:Article");
+        $article = $repo->find(1);
+
+        return $this->render("article/article.html.twig", ['article' => $article]);
+    }
+
+
+    /**
+     * @Route("/backoffice", name="backoffice")
+     */
+    public function backofficeAction()
+    {
+        $repo = $this->getDoctrine()->getRepository("AppBundle:Article");
+        $articles = $repo->findAll();
+
+        return $this->render("article/backoffice.html.twig", ['articles' => $articles]);
+    }
+
 }
+ 
---
+++ src/AppBundle/Form/Type/CreateUpdateFormType.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CreateUpdateFormType extends AbstractType
+{
+	public function BuildForm(FormBuilderInterface $builder, array $options)
+	 {
+	 	$builder
+            ->add('name', TextType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+	 }
+}
